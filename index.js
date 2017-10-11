6e342a0b28e45373e7ab4b4a0670d1ebcc316684
tredwood89 <tredwood89@gmail.com>
2017-10-11 03:13:06 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,3 +4,10 @@ function updateObjectWithKeyAndValue(object, key, value) {
 
   return Object.assign ({}, object, { [key]:value })
 }
+
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
+
+    object[key]=value
+
+  return object  
+}
