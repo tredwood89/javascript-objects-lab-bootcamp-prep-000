21d08e987ea8caf371304cb0c661ed9532f00fa9
tredwood89 <tredwood89@gmail.com>
2017-10-11 04:15:40 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,10 +12,4 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
   return object
 }
 
-function deleteFromObjectByKey(object, key) {
 
-    delete object[key]
-
-  return object
-
-}
